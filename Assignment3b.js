6720f382ac11f1adee0845c8b071fc014d56dd2a
BenKamotho <105359629+BernardKamotho@users.noreply.github.com>
2025-05-29 10:53:34 +0300

    modified some files

---
+++ Assignment3b.js
@@ -0,0 +1,3 @@
+// create a js program that checks if a number entered is positive or negative or zero
+
+// Read and make some short notes on comparison operators in js, logical operators and assignment operators in js.
